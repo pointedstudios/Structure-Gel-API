cbc0453e5b7a3a04f71f2b797403daaf340aee54
Silver_David <djbake101@gmail.com>
2020-04-24 12:11:57 -0400

    Added config templates for structures. You can use these to automatically handle generating config settings.

---
+++ src/main/java/com/legacy/structure_gel/ConfigTemplates.java
@@ -0,0 +1,134 @@
+package com.legacy.structure_gel;
+
+import java.util.Arrays;
+
+import com.google.common.collect.ImmutableList;
+import com.legacy.structure_gel.structures.ProbabilityStructure;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biomes;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ConfigTemplates
+{
+	/**
+	 * Useful in conjunction with {@link ProbabilityStructure} to make generation
+	 * settings configurable.
+	 * 
+	 * @author David
+	 *
+	 */
+	public static class StructureConfig
+	{
+		private final ForgeConfigSpec.DoubleValue probability;
+		private final ForgeConfigSpec.IntValue spacing;
+		private final ForgeConfigSpec.IntValue offset;
+
+		/**
+		 * 
+		 * @param builder
+		 * @param name
+		 * @param defaultProbability
+		 * @param defaultSpacing
+		 * @param defaultOffset
+		 */
+		public StructureConfig(ForgeConfigSpec.Builder builder, String name, double defaultProbability, int defaultSpacing, int defaultOffset)
+		{
+			this.probability = builder.comment("Chance of generating in an allowed chunk").defineInRange(name + ".probability", defaultProbability, 0.0D, 1.0D);
+			this.spacing = builder.comment("Spacing between structures").defineInRange(name + ".spacing", defaultSpacing, 1, Integer.MAX_VALUE);
+			this.offset = builder.comment("Offsets the spacing of the structures randomly").defineInRange(name + ".offset", defaultOffset, 0, Integer.MAX_VALUE);
+		}
+
+		public double getProbability()
+		{
+			return this.probability.get();
+		}
+
+		public int getSpacing()
+		{
+			return this.spacing.get();
+		}
+
+		public int getOffset()
+		{
+			return this.offset.get();
+		}
+	}
+
+	/**
+	 * Extension of {@link StructureConfig} that allows allows stores what biomes a
+	 * structure should generate in. Use this setting in {@link FMLCommonSetupEvent}
+	 * to register the structure in the appropriate biomes.
+	 * 
+	 * @author David
+	 *
+	 */
+	public static class BiomeStructureConfig extends StructureConfig
+	{
+		private final ForgeConfigSpec.ConfigValue<String> biomeString;
+		private ImmutableList<Biome> biomes = ImmutableList.of();
+
+		/**
+		 * 
+		 * @param builder
+		 * @param name
+		 * @param defaultProbability
+		 * @param defaultSpacing
+		 * @param defaultOffset
+		 * @param defaultBiomesString : Entered as a comma separated string of resource locations. You can put spaces, but you don't need to. Ex: "plains, minecraft:swamp, biomesoplenty:origin_beach"
+		 */
+		public BiomeStructureConfig(ForgeConfigSpec.Builder builder, String name, double defaultProbability, int defaultSpacing, int defaultOffset, String defaultBiomesString)
+		{
+			super(builder, name, defaultProbability, defaultSpacing, defaultOffset);
+			this.biomeString = builder.comment("Biomes that this can generate in, separated by comma. Spaces are allowed but not needed.").define(name + ".biomes", defaultBiomesString);
+			FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onConfigLoad);
+		}
+
+		/**
+		 * Converts {@link #biomeString} to a list of {@link Biome} that can be checked
+		 * when adding structures to a biome to see which biomes it can be added to.
+		 * Automatically called on {@link ModConfig.ModConfigEvent} to refresh the list
+		 * when the config changes.<br>
+		 * <br>
+		 * Invalid biome IDs are defaulted to minecraft:plains
+		 * 
+		 * @param event
+		 */
+		@SubscribeEvent
+		protected void onConfigLoad(ModConfig.ModConfigEvent event)
+		{
+			this.biomes = Arrays.asList(this.biomeString.get().replace(" ", "").split(",")).stream().map(s ->
+			{
+				if (ForgeRegistries.BIOMES.containsKey(new ResourceLocation(s)))
+					return ForgeRegistries.BIOMES.getValue(new ResourceLocation(s));
+				else
+				{
+					StructureGelMod.LOGGER.warn(String.format("The biome \"%s\" entered in the config %s is not a registered biome. Defaulting to plains.", s, this.biomeString.getPath()));
+					return Biomes.PLAINS;
+				}
+			}).collect(ImmutableList.toImmutableList());
+		}
+
+		/**
+		 * Gets the list of biomes that a structure is allowed to spawn in.<br>
+		 * <br>
+		 * You can call this during {@link FMLCommonSetupEvent} to register only to
+		 * biomes in the config (requires restart, but slightly more optimal for
+		 * performance since it only happens on startup), or when the structure checks
+		 * to see if it can be placed in a given chunk (no restart required, but
+		 * potentially worse performance since it happens every time a chunk generates)
+		 * 
+		 * @return {@link ImmutableList}
+		 */
+		public ImmutableList<Biome> getBiomes()
+		{
+			return this.biomes;
+		}
+	}
+}
--- src/main/java/com/legacy/structure_gel/StructureGelMod.java
+++ src/main/java/com/legacy/structure_gel/StructureGelMod.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Streams;
 import com.legacy.structure_gel.blocks.IStructureGel.Behavior;
@@ -41,6 +44,7 @@ public class StructureGelMod
 	public static final String NAME = "Structure Gel API";
 	public static final String MODID = "structure_gel";
 	public static final String VERSION = "1.0.0";
+	public static final Logger LOGGER = LogManager.getLogger();
 
 	public StructureGelMod()
 	{
