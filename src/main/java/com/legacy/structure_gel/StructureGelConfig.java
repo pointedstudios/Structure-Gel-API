c3a72a13b05ac30d9221b7b6961d5fcfdab156c6
Silver_David <djbake101@gmail.com>
2020-06-29 21:08:01 -0400

    Updated version number

--- src/main/java/com/legacy/structure_gel/StructureGelConfig.java
+++ src/main/java/com/legacy/structure_gel/StructureGelConfig.java
@@ -2,10 +2,6 @@ package com.legacy.structure_gel;
 
 import org.apache.commons.lang3.tuple.Pair;
 
-import com.google.common.collect.ImmutableMap;
-import com.legacy.structure_gel.util.ConfigTemplates;
-
-import net.minecraft.entity.EntityClassification;
 import net.minecraftforge.common.ForgeConfigSpec;
 
 public class StructureGelConfig
@@ -26,8 +22,6 @@ public class StructureGelConfig
 		public Common(ForgeConfigSpec.Builder builder)
 		{
 			this.extraLakeProofing = builder.comment("Adds more vanilla structures to the list of structures that lakes cannot generate inside of. Only villages when set to false. Requires reload.").define("features.extra_lake_proofing", true);
-			new ConfigTemplates.StructureConfigBuilder(builder, "test").probability(1.0).spacing(4).offset(2).biomes(true, "plains, forest").spawns(ImmutableMap.of(EntityClassification.MONSTER, "[minecraft:zombie, 1, 2, 3]", EntityClassification.AMBIENT, "[minecraft:bat, 3, 4, 5]"));
-			new ConfigTemplates.StructureConfigBuilder(builder, "example").spacing(5);
 		}
 
 		public boolean getExtraLakeProofing()
